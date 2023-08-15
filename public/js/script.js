fd4d1571155aedfc9be5160249fcb92041c35c79
ccyr <ccyrius01@gmail.com>
2023-08-14 21:45:03 -0600

    css path  in layout corrected

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,14 @@
 
 'use strict';
 
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
 /**
  * Handle mobile menu functionality to hide/reveal sidebar on mobile layouts
  */
