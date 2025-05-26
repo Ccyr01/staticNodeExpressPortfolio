2e33dfe393fed2046bc9de665d3abf6b4348928e
Christian Cyr <ccyr01@nmsu.edu>
2025-05-25 21:57:47 -0600

    Updated root route back to original

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,30 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const data = require('../data.json');
+const data = require("../data.json");
 
 console.log(data);
 
-// router.get('/', function(req, res, next) {
-//     res.render('index', { projects: data.projects  });
+router.get("/", function (req, res, next) {
+  res.render("index", { projects: data.projects });
+});
+// router.get('/', (req, res) => {
+//   console.log('Root route hit!');
+//   res.send('Hello from root!');
 // });
-router.get('/', (req, res) => {
-  console.log('Root route hit!');
-  res.send('Hello from root!');
+
+router.get("/about", (req, res) => {
+  res.render("about");
 });
 
-
-router.get('/about', (req, res) => {
-    res.render('about');
-});
-
-router.get('/projects/:id', function(req, res, next) {
-    const projectId = req.params.id;
-    const project = data.projects.find( ({ id }) => id === +projectId );
-    console.log("project id" + projectId);
-    if (project) {
-      res.render('project', { project });
-    } else {
-      res.sendStatus(404);
-    }
+router.get("/projects/:id", function (req, res, next) {
+  const projectId = req.params.id;
+  const project = data.projects.find(({ id }) => id === +projectId);
+  console.log("project id" + projectId);
+  if (project) {
+    res.render("project", { project });
+  } else {
+    res.sendStatus(404);
+  }
 });
 
 module.exports = router;
